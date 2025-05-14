832193930cd1cb13ca681d2d0d28e22fd125d28a
UzairAhmadKhattak <uzair3368@gmail.com>
2025-05-14 17:39:41 +0500

    seperate crud from router

--- src/accounts/routers.py
+++ src/accounts/routers.py
@@ -5,44 +5,29 @@ from fastapi import Depends, HTTPException
 from ..session import get_db
 from .models import User,Role
 from .constants import UserConstants
-from passlib.context import CryptContext
+from .cruds import get_password_hash,UserCrud
 
-router = APIRouter(prefix='/accounts')
+user_crud = UserCrud()
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
+router = APIRouter(prefix='/accounts',tags=['Accounts'])
 
 
 @router.post('/customer_registration',response_model=UserRead)
 async def customer_registration(user: UserCreate, db:Session = Depends(get_db)):
     
-    db_user = db.query(User).filter(User.username == user.username).first()
+    db_user = user_crud.get_user_by_username(db,user.username)
     if db_user:
         raise HTTPException(status_code=400, detail= UserConstants.USER_EXISTS.value)
     
     hashed_password = get_password_hash(user.password)
-    role = db.query(Role).filter(Role.name==UserConstants.CUSTOMER_ROLE.value["name"])
-    if not role:
-        role = Role(name=UserConstants.CUSTOMER_ROLE.value["name"],
-                     description=UserConstants.CUSTOMER_ROLE.value["description"])
-        db.add(role)
-        db.commit()
-        db.refresh(role)
-    else:
-        role = role.first()
 
-    db_user = User(username=user.username, 
+    role = user_crud.get_or_create_user_role(db,UserConstants.CUSTOMER_ROLE)
+
+    db_user = user_crud.create_customer(username=user.username, 
                    email=user.email, 
                    full_name=user.full_name, 
                    hashed_password=hashed_password, 
                    is_active=True,role_id=role.id)
-    
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
     return db_user
 
     
