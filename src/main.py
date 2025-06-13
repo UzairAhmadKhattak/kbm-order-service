7b596730664d5e72b80a18f177f6497062d8d995
UzairAhmadKhattak <uzair3368@gmail.com>
2025-06-13 14:23:21 +0500

    update how the env is getting

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from src.accounts.routers import router
 from dotenv import load_dotenv
+from fastapi.responses import PlainTextResponse
+
 
 load_dotenv()
 
 app = FastAPI()
+
+@app.get("/health", response_class=PlainTextResponse)
+async def health_check():
+    return "OK"
+ 
 app.include_router(router, prefix='/api/v1')
